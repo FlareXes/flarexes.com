30c0bd9a762989c806ce56e7d0d4d69cc8eba2ef
FlareXes <70066527+FlareXes@users.noreply.github.com>
2023-09-22 14:43:50 +0530

    Added Post Model With Rich Text Editor (TinyMCE)

--- APP_BLOG/admin.py
+++ APP_BLOG/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from APP_BLOG.models import Post
+
+
 # Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    class Media:
+        js = ('APP_BLOG/js/tinymce.js',)
--- APP_BLOG/models.py
+++ APP_BLOG/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True)
+    content = models.TextField()
+    thumbnail = models.ImageField(blank=True, null=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
